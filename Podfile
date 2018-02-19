96700e90e4aac7b543998e556f3eb33f6de866b9
Andy Graulund <andreas@graulund.com>
2018-02-19 20:21:12 +0100

    Using Cocoapods

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :osx, '10.10'
+use_frameworks!
+
+target 'Clip Manager' do
+	pod 'AMCoreAudio', '~> 3.1'
+end
---
+++ Pods/Target Support Files/Pods-Clip Manager/Pods-Clip Manager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Clip_Manager : NSObject
+@end
+@implementation PodsDummy_Pods_Clip_Manager
+@end
